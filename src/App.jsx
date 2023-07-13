efbf0ec344ba29e3a2cae95e1f27d223f9a8f942
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-07-13 20:16:37 +0100

    Bug fix - import preline library in entry point for app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import { Navbar } from "./components/Navbar";
 import Projects from "./components/Projects";
+import Example from "./components/Test";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <About />
         <Projects />
         <Contact />
+        {/* <Example /> */}
       </main>
       <Footer />
     </div>
